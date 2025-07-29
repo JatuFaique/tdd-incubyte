248252df0869ef4fc642d970e2bb703538545a3e
JatuFaique <jatu631@gmail.com>
2025-07-29 23:36:34 +0530

    test(sum): test parameter to be empty string

--- index.test.js
+++ index.test.js
@@ -3,3 +3,13 @@ import { sum } from './index.js';
 test('adds "1,2" to equal 3', () => {
   expect(sum("1,2")).toBe(3);
 });
+
+// add any number of numbers
+test('adds "1,2,3" to equal 6', () => {
+  expect(sum("1,2,3")).toBe(6);
+});
+
+// test Input: “”, Output: 0
+test('adds "" to equal 0', () => {
+  expect(sum("")).toBe(0);
+});
