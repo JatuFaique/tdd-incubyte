6b24a97fea9d30d86634130c8352d29a9fa304f7
JatuFaique <jatu631@gmail.com>
2025-07-30 00:11:57 +0530

    test(add): added number extractor from string method

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ export function add(parameter) {
     }
     // Check if the parameter is a string and split it by commas then parse as int
     if (typeof parameter === 'string') {
-        return parameter.split(',').reduce((acc, num) => acc + parseInt(num, 10), 0);
+        return getNumbersFromString(parameter).reduce((acc, num) => acc + num, 0);
     }
+}
+
+/**
+ * Parses a string of numbers separated by commas or newlines and returns an array of integers.
+ * @param {string} str - The input string containing numbers.
+ * @returns {number[]} - An array of integers parsed from the input string.
+ */
+function getNumbersFromString(str) {
+    return str.split(/[\n,]+/).map(num => parseInt(num, 10)).filter(num => !isNaN(num));
 }
