97248863df57879d941b2a9fbc10586eda721ec6
JatuFaique <jatu631@gmail.com>
2025-07-29 23:30:52 +0530

    test(sum): reading string and adding the digits seperated by comma

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 export function sum(parameter) {
-  return
+    // Check if the parameter is a string and split it by commas then parse as int
+    if (typeof parameter === 'string') {
+        return parameter.split(',').reduce((acc, num) => acc + parseInt(num, 10), 0);
+    }
 }
